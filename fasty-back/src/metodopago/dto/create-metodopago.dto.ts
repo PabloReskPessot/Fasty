ea9e29f9d5d1bd40c5065f588f0d270e36fdb467
melina izaguirre <melinaizaguirre@hotmail.com>
2025-11-08 23:10:43 -0300

    metodo de pago y los disponibles

--- fasty-back/src/metodopago/dto/create-metodopago.dto.ts
+++ fasty-back/src/metodopago/dto/create-metodopago.dto.ts
@@ -1,1 +1,14 @@
-export class CreateMetodopagoDto {}
+import { IsBoolean, IsInt, IsNotEmpty } from 'class-validator';
+
+export class CreateMetodopagoDto {
+  @IsInt()
+  @IsNotEmpty()
+  usuarioID: number;
+
+  @IsInt()
+  @IsNotEmpty()
+  metodoPagoDisponibleID: number;
+
+  @IsBoolean()
+  predeterminado: boolean;
+}
--- fasty-back/src/metodopagodisponible/metodopagodisponible.service.ts
+++ fasty-back/src/metodopagodisponible/metodopagodisponible.service.ts
@@ -1,26 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { MetodoPagoDisponible } from './entities/metodopagodisponible.entity';
 import { CreateMetodopagodisponibleDto } from './dto/create-metodopagodisponible.dto';
 import { UpdateMetodopagodisponibleDto } from './dto/update-metodopagodisponible.dto';
 
 @Injectable()
 export class MetodopagodisponibleService {
-  create(createMetodopagodisponibleDto: CreateMetodopagodisponibleDto) {
-    return 'This action adds a new metodopagodisponible';
+  constructor(
+    @InjectRepository(MetodoPagoDisponible)
+    private readonly metodoPagoDisponibleRepository: Repository<MetodoPagoDisponible>,
+  ) {}
+
+  async create(createDto: CreateMetodopagodisponibleDto) {
+    const nuevoMetodo = this.metodoPagoDisponibleRepository.create(createDto);
+    return await this.metodoPagoDisponibleRepository.save(nuevoMetodo);
   }
 
-  findAll() {
-    return `This action returns all metodopagodisponible`;
+  async findAll() {
+    return await this.metodoPagoDisponibleRepository.find({
+      relations: ['metodosPago'], // Para ver los métodos asociados si querés
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} metodopagodisponible`;
+  async findOne(id: number) {
+    const metodo = await this.metodoPagoDisponibleRepository.findOne({
+      where: { metodoPagoDisponibleID: id },
+      relations: ['metodosPago'],
+    });
+
+    if (!metodo) {
+      throw new NotFoundException(`Método de pago ${id} no encontrado`);
+    }
+
+    return metodo;
   }
 
-  update(id: number, updateMetodopagodisponibleDto: UpdateMetodopagodisponibleDto) {
-    return `This action updates a #${id} metodopagodisponible`;
+  async update(id: number, updateDto: UpdateMetodopagodisponibleDto) {
+    const metodo = await this.metodoPagoDisponibleRepository.preload({
+      metodoPagoDisponibleID: id,
+      ...updateDto,
+    });
+
+    if (!metodo) {
+      throw new NotFoundException(`Método de pago ${id} no encontrado`);
+    }
+
+    return await this.metodoPagoDisponibleRepository.save(metodo);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} metodopagodisponible`;
+  async remove(id: number) {
+    const metodo = await this.metodoPagoDisponibleRepository.findOneBy({ metodoPagoDisponibleID: id });
+    if (!metodo) {
+      throw new NotFoundException(`Método de pago ${id} no encontrado`);
+    }
+
+    await this.metodoPagoDisponibleRepository.remove(metodo);
+    return { message: `Método de pago ${id} eliminado correctamente` };
   }
 }
